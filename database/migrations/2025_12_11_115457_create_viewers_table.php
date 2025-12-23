0da4fb71db8c479042e48e2f40022bb1960bfddb
Mutahar Nouman | baaboo FZCO <mutahar.nouman@baaboo.com>
2025-12-23 07:51:06 +0400

    changeOFMigration

--- database/migrations/2025_12_11_115457_create_viewers_table.php
+++ database/migrations/2025_12_11_115457_create_viewers_table.php
@@ -19,8 +19,8 @@ return new class extends Migration
             $table->longText("user_agent")->nullable();
             $table->string("location")->nullable();
             $table->longText("session_id")->nullable();
-            $table->time("created_at")->nullable()->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->time("updated_at")->nullable()->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp("created_at")->nullable()->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp("updated_at")->nullable()->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
