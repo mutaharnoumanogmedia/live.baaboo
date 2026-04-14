83263864e061a853aba0359653f9a128d1d673a9
Mutahar Nouman | baaboo FZCO <mutahar.nouman@baaboo.com>
2026-04-14 14:35:24 +0400

    changeOfQueue

--- app/Jobs/SendWinnerEmailJob.php
+++ app/Jobs/SendWinnerEmailJob.php
@@ -2,23 +2,28 @@
 
 namespace App\Jobs;
 
+use App\Mail\WinnerNotificationMail;
+use App\Models\LiveShow;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\WinnerNotificationMail;
-use App\Models\User;
-use App\Models\LiveShow;
 
 class SendWinnerEmailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
     public int $userId;
+
     public string $prizeWon;
+
     public LiveShow $liveShow;
+
+  
+
     public function __construct(int $userId, string $prizeWon, LiveShow $liveShow)
     {
         $this->userId = $userId;
