d9b5c91d143c2611c2c8ae8291637ef2656c61d1
Mutahar Nouman | baaboo FZCO <mutahar.nouman@baaboo.com>
2026-04-22 12:09:07 +0400

    fixed

--- app/Models/UserLiveShow.php
+++ app/Models/UserLiveShow.php
@@ -27,6 +27,22 @@ class UserLiveShow extends Pivot
     // change the score , calculate from the user_quiz_responses table sum of response_score
     public function getScoreAttribute()
     {
-        return $this->userQuizResponses()->sum('response_score');
+        return round($this->userQuizResponses()->sum('response_score'), 2);
+    }
+    public function userQuizResponses()
+    {
+        return $this->hasMany(UserQuizResponse::class, 'user_id', 'user_id');
+    }
+    public function userQuiz()
+    {
+        return $this->hasOne(UserQuiz::class, 'user_id', 'user_id');
+    }
+    public function liveShow()
+    {
+        return $this->belongsTo(LiveShow::class, 'live_show_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
