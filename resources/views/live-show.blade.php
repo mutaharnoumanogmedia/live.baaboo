5a24b136b0ceb519ab25f6f5c0de8ba895f17e8e
Mutahar Nouman | baaboo FZCO <mutahar.nouman@baaboo.com>
2026-04-23 21:54:22 +0400

    cchch

--- resources/views/live-show.blade.php
+++ resources/views/live-show.blade.php
@@ -1617,7 +1617,7 @@
 
                         bar.style.background = '#28a745'; // Green for correct
                         //make the option label border green
-                        label.style.borderColor = '#28a745';
+                        label.parentElement.style.borderColor = '#28a745';
                         // label.style.color = '#28a745';
                     }
                 } catch (e) {
