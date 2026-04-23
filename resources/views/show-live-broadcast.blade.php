1b32f769dc183db311dcab2b4e9e3f60a58b17e1
Mutahar Nouman | baaboo FZCO <mutahar.nouman@baaboo.com>
2026-04-23 15:19:52 +0400

    change360P

--- resources/views/show-live-broadcast.blade.php
+++ resources/views/show-live-broadcast.blade.php
@@ -129,7 +129,8 @@
         const zp = ZegoUIKitPrebuilt.create(TOKEN);
         zp.joinRoom({
             container: document.querySelector("#root"),
-
+            videoResolutionList: [ZegoUIKitPrebuilt.VideoResolution_360P],
+            videoResolutionDefault: ZegoUIKitPrebuilt.VideoResolution_360P,
             scenario: {
                 mode: ZegoUIKitPrebuilt.LiveStreaming,
                 config: {
